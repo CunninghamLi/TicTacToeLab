f9fc1e08a450ed30aa13a5d93ae3035f10da0838
Cunningham Li <2231358@champlaincollege.qc.ca>
2024-11-06 12:04:06 -0500

    Add project files.

---
+++ TicTacToe/Startup.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for Startup.xaml
+    /// </summary>
+    public partial class Startup : Window
+    {
+        public Startup()
+        {
+            InitializeComponent();
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            img2.Source = 
+                new BitmapImage(new Uri("Images/tic-tac-toe_x.png", UriKind.Relative));
+        }
+    }
+}
