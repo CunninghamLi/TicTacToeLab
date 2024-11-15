cf3e1da266d0774fc592d6f7bb3c0b130f9b7890
Cunningham Li <2231358@champlaincollege.qc.ca>
2024-11-14 22:13:44 -0500

    .

---
+++ TicTacToe/Board.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Board
+    {
+        public enum Player { None, X, O }
+        private Player[,] grid = new Player[3, 3];
+        public Player CurrentPlayer { get; private set; } = Player.X;
+
+        public bool Select(int row, int column)
+        {
+            if (grid[row, column] != Player.None) return false;
+
+            grid[row, column] = CurrentPlayer;
+            CurrentPlayer = (CurrentPlayer == Player.X) ? Player.O : Player.X;
+            return true;
+        }
+
+        public Player CheckWin()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (grid[i, 0] != Player.None && grid[i, 0] == grid[i, 1] && grid[i, 1] == grid[i, 2]) return grid[i, 0];
+                if (grid[0, i] != Player.None && grid[0, i] == grid[1, i] && grid[1, i] == grid[2, i]) return grid[0, i];
+            }
+
+            if (grid[0, 0] != Player.None && grid[0, 0] == grid[1, 1] && grid[1, 1] == grid[2, 2]) return grid[0, 0];
+            if (grid[0, 2] != Player.None && grid[0, 2] == grid[1, 1] && grid[1, 1] == grid[2, 0]) return grid[0, 2];
+
+            return Player.None;
+        }
+
+        public void ResetBoard()
+        {
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    grid[i, j] = Player.None;
+
+            CurrentPlayer = Player.X;
+        }
+    }
+}
+
--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -15,14 +15,58 @@ using System.Windows.Shapes;
 
 namespace TicTacToe
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Board gameBoard = new Board();
+
         public MainWindow()
         {
             InitializeComponent();
+            UpdateTurnLabel();
+        }
+
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Image clickedImage = sender as Image;
+            if (clickedImage == null) return;
+
+            string[] position = clickedImage.Tag.ToString().Split(',');
+            int row = int.Parse(position[0]);
+            int column = int.Parse(position[1]);
+
+            if (gameBoard.Select(row, column))
+            {
+                clickedImage.Source = new BitmapImage(new Uri(gameBoard.CurrentPlayer == Board.Player.O ? "images/tic-tac-toe_x.png" : "images/tic-tac-toe_o.png", UriKind.Relative));
+
+                var winner = gameBoard.CheckWin();
+                if (winner != Board.Player.None)
+                {
+                    MessageBox.Show($"{winner} wins!");
+                    ResetGame();
+                }
+                else
+                {
+                    UpdateTurnLabel();
+                }
+            }
+        }
+
+        private void UpdateTurnLabel()
+        {
+            TurnLabel.Text = $"Turn: Player {(gameBoard.CurrentPlayer == Board.Player.X ? "X" : "O")}";
+        }
+
+        private void ResetGame()
+        {
+            gameBoard.ResetBoard();
+            foreach (UIElement element in MainGrid.Children)
+            {
+                if (element is Image img)
+                {
+                    img.Source = new BitmapImage(new Uri("images/tic-tac-toe_1181538.png", UriKind.Relative));
+                }
+            }
+            UpdateTurnLabel();
         }
     }
 }
